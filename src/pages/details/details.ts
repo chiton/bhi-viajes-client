b3006b86885ef703caf66315079986c7ddf7eb5d
chiton <adrian.h.anton@gmail.com>
2018-07-31 18:41:50 -0300

    Add env

--- src/pages/details/details.ts
+++ src/pages/details/details.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { Viaje } from '../../models/viaje';
-import { Geolocation } from '@ionic-native/geolocation';
-import { NativeGeocoder, NativeGeocoderReverseResult } from '@ionic-native/native-geocoder';
 
 // We MUST import both the firebase AND firestore modules like so
 import * as firebase from 'firebase';
@@ -22,11 +20,8 @@ export class DetailsPage {
 
   constructor(public navCtrl: NavController, 
     private navParams: NavParams,
-    private geolocation: Geolocation,
-    private nativeGeocoder: NativeGeocoder,
     private mapProvider: MapProvider,
     private dataProvider: DataProvider,
-    private alertCtl: AlertController,
     private admob: AdMobFree) {
     this.viaje = this.navParams.get('viaje');
   }
@@ -34,71 +29,8 @@ export class DetailsPage {
   ionViewDidLoad() {
     this.mapProvider.loadMap(this.viaje.coord.latitude, this.viaje.coord.longitude);
 
-    // Get coord details street etc, test in device
-    this.nativeGeocoder.reverseGeocode(this.viaje.coord.latitude, this.viaje.coord.longitude)
-      .then((result: NativeGeocoderReverseResult) => console.log(JSON.stringify(result)))
-      .catch((error: any) => console.log(error));
-
     this.showBanner();
   }
-
-  getPosition(): any {
-    this.geolocation.getCurrentPosition()
-    .then((response) => {
-      this.mapProvider.loadMap(response.coords.latitude, response.coords.longitude);
-    })
-    .catch((error) =>{
-      console.log(error);
-    })}
-
-  informCoord(viaje : Viaje) : any {
-    this.geolocation.getCurrentPosition()
-    .then((response) => {
-        this.viaje.coord = new firebase.firestore.GeoPoint(response.coords.latitude, response.coords.longitude);
-        this.viaje.lastEntry = firebase.firestore.Timestamp.fromDate(new Date());
-        
-        this.dataProvider.updateViaje(this.viaje);
-        this.mapProvider.loadMap(this.viaje.coord.latitude, this.viaje.coord.longitude);
-      })
-    .catch((error) =>{
-        console.log(error);
-      })
-    }
-
-    removeViaje(viaje : Viaje)
-    {
-      this.showConfirmationMessage(viaje);
-    }
-
-    editViaje(viaje : Viaje) {
-      this.navCtrl.pop();
-      this.navCtrl.push(EditorPage, {
-        viaje: viaje});
-    }
-  
-
-    showConfirmationMessage(viaje : Viaje) {
-      let alert = this.alertCtl.create({
-        title: 'Confirmar eliminación',
-        message: 'Estás seguro que querés eliminarlo?',
-        buttons: [
-          {
-            text: 'Cancelar',
-            role: 'cancel',
-            handler: () => {
-            }
-          },
-          {
-            text: 'Eliminar',
-            handler: () => {
-              this.dataProvider.removeViaje(viaje);
-              this.navCtrl.pop();
-            }
-          }
-        ]
-      });
-      alert.present();
-    }
 
     showBanner() {
       let bannerConfig: AdMobFreeBannerConfig = {
