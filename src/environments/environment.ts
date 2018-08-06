ee50429d83155c12d52db020333cc98dda9234ca
Adrian Anton <adrian.h.anton@gmail.com>
2018-08-06 18:21:43 -0300

    Update environment.ts
    Update env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
     production: false,
     firebase: {
-    apiKey: "AIzaSyDEODatSkrEFAGBS_xGnKHOweieWTRb2ZE",
-    authDomain: "bhiviajes-1527989989262.firebaseapp.com",
-    databaseURL: "https://bhiviajes-1527989989262.firebaseio.com/",
-    projectId: "bhiviajes-1527989989262",
-    storageBucket: "bhiviajes-1527989989262.appspot.com/",
-    messagingSenderId: "903778168776"
+    apiKey: "XXX",
+    authDomain: "XXX",
+    databaseURL: "XXX",
+    projectId: "XXX",
+    storageBucket: "XXXX,
+    messagingSenderId: "XXX"
   }
 };
